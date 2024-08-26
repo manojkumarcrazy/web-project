6613a0658f9903e58dd220e0cb7fb0f7df3403f5
Manoj Kumar <125989430+manojkumarcrazy@users.noreply.github.com>
2024-08-26 15:49:27 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,79 @@
+
+   
+        //   Arithmetic Operators
+
+
+    //    1.Addition Operator
+        
+   
+    let a = 1;
+    let b = 2;
+    let sum = a + b;
+   document.getElementById("p").innerHTML= "The Add two value is "+ sum;
+              
+ // 2.Subtraction operator
+
+  let c = 4;
+  let d = 5;
+  let sub = c - d;
+  document.getElementById("p1").innerHTML= "The Subtraction two value is "+ sub;
+
+ // 3.	Multiplication operators
+
+ let e = 4;
+  let f= 5;
+  let mul = e * f;
+  document.getElementById("p2").innerHTML= "The Multiplication two value is "+ mul;
+
+  // 4.Division operators 
+
+  let g = 4;
+  let h = 5;
+  let div = g /h;
+  document.getElementById("p3").innerHTML= "The Division two value is "+ div;
+  
+    // 5.Modulus operator
+
+    let i = 10;
+    let j = 3;
+    let mod = i % j;
+    document.getElementById("p4").innerHTML= "The Modulus two value is "+ mod;
+
+    // 6.Exponentiation operators
+
+    let k = 2;
+    let l = 5;
+    let exp = Math.pow(k, l);
+    document.getElementById("p5").innerHTML= "The Exponentiation  two value is "+ exp;
+  
+    // 7.Increment operator
+
+    let m = 10;
+    // m++;
+    ++m;
+    let inc=m;
+    document.getElementById("p6").innerHTML= "The Increment two value is "+ inc;
+
+
+    // 8.Decrement operator
+
+    let n = 10;
+    // m++;
+    --n;
+    let dec=n;
+    document.getElementById("p7").innerHTML= "The Decrement two value is "+ dec;
+
+
+     // Assignment Operators
+
+ // 1. Is Equal 
+
+ let ab = 1;
+    let cd =1;
+    let equ = ab==cd;
+   document.getElementById("p10").innerHTML= "The value is equal or not " +  (equ);
+   
+              
+
+
+    
